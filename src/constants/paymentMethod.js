90aa74acede3a0d3eda4d5be7ca621fb64c3bbac
Duong Hoai Bao <dhbao2505@gmail.com>
2025-03-01 02:47:40 +0700

    refactor(constants): update constants

--- src/constants/paymentMethod.js
+++
@@ -1,6 +0,0 @@
-const PAYMENT_METHOD = {
-    cod: { id: 1, label: "Thanh toán khi nhận hàng", color: "green" },
-    vnpay: { id: 2, label: "Thanh toán qua VNPay", color: "gray" },
-  };  
-
-export default PAYMENT_METHOD;

c860864874dc3077c45335822b2492c8b46d41b9
Duong Hoai Bao <dhbao2505@gmail.com>
2025-04-04 16:38:57 +0700

    feat(socket): add socket provider

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,19 +2,22 @@ import ScrollToTop from "../components/common/ScrollToTop";
 import Header from "../components/common/Header";
 import Sidebar from "../components/common/SideBar";
 import PropTypes from "prop-types";
+import SocketProvider from "../context/SocketProvider";
 
 const MainLayout = ({ children }) => {
   return (
-    <div className="flex flex-row container w-full min-h-screen">
-      <div className="w-1/5 h-screen">
-        <Sidebar />
+    <SocketProvider>
+      <div className="flex flex-row container w-full min-h-screen">
+        <div className="w-1/5 h-screen">
+          <Sidebar />
+        </div>
+        <div className="flex flex-col w-4/5 h-screen">
+          <Header />
+          <ScrollToTop />
+          <div className="flex-grow overflow-y-auto">{children}</div>
+        </div>
       </div>
-      <div className="flex flex-col w-4/5 h-screen">
-        <Header />
-        <ScrollToTop />
-        <div className="flex-grow overflow-y-auto">{children}</div>
-      </div>
-    </div>
+    </SocketProvider>
   );
 };
 

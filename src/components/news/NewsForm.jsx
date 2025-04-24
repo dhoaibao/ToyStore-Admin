8a3a35ad1e8cd0979531da0262d702470791fefb
Duong Hoai Bao <dhbao2505@gmail.com>
2025-04-24 18:23:05 +0700

    refactor: news

--- src/components/news/NewsForm.jsx
+++ src/components/news/NewsForm.jsx
@@ -66,6 +66,7 @@ const NewsForm = ({ open, setOpen, data, setFetchData }) => {
     setOpen(false);
     form.resetFields();
     setFileList([]);
+    setContent("");
   };
 
   const onFinish = async (values) => {
